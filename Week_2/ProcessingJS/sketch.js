60a15d6fc3802cadad836dbd5e75cfa74b603973
peachy-sky <fluffypuppyw5@gmail.com>
2018-07-03 14:16:50 -0700

    user can choose the canvas size, and circles do not leave trails now

--- Week_2/ProcessingJS/sketch.js
+++ Week_2/ProcessingJS/sketch.js
@@ -1,5 +1,5 @@
 //define an object that describes a circle
-var circle = {
+/* var circle = {
 	diameter: 20,
 	xCoor: 0,
 	yCoor: 0,
@@ -8,63 +8,54 @@ var circle = {
 	xSpeed: 8,
 	ySpeed: 8
 };
+*/
 
-
-
-//setup is run once at the start
-function setup() {
-	createCanvas(1300, 680);
-	frameRate(15);
+function Ball(x, y, diameter, color, xSpeed, ySpeed){
+	this.xCoor = x;																		//"this" is a keyword that refers to the individual object
+	this.yCoor = y;
+	this.diameter = diameter;
+	this.color = color || [0, 0, 0];													// || [0, 0, 0] makes the default color black
+	this.xSpeed = xSpeed || 12;
+	this.ySpeed = ySpeed || 5;
 }
 
+var circle = new Ball(30, 30, 15);														//define global variables
+var circle2 = new Ball(30, 30, 15);
+var circle3 = new Ball(30, 30, 15);
+var ballList = [circle, circle2, circle3];
+
+
+function setup() {																		//setup is run once at the start
+	createCanvas(parseInt(prompt("What do you want the canvas's X length to be?")), parseInt(prompt("What do you want the canvas's Y length to be?")));																			//ask what size the user wants the window to be
+}
+
+
+function randomColor() {
+	return [random(0,256), random(0, 256), random(0, 256)];
+}
 
 
 //draw the circle
 function draw() {
-    fill(circle.color);
- 	ellipse(circle.xCoor, circle.yCoor, circle.diameter);
-	
-	//if hit the right/left edge of the canvas
-	
-	if(circle.xCoor >= 1300){	
-		//makes the circle a random color
-		circle.color = [random(0, 255), random(0, 255), random(0, 255)];
-		
-		//change the circle speed to make it move left
-		circle.xSpeed = -circle.xSpeed;
-		
-	}else if(circle.xCoor < 0) {
-		
-		//make the circle a random color
-		circle.color = [random(0, 255), random(0, 255), random(0, 255)];
-		
-		//change the circle speed to make it move right
-		circle.xSpeed = -circle.xSpeed;
-		
+	background(255,255,255);
+	for(var i = 0; i < ballList.length; i++){
+		fill(ballList[i].color);
+		ellipse(ballList[i].xCoor, ballList[i].yCoor, ballList[i].diameter);
+		if(ballList[i].xCoor >= canvas.width){											//if hit the right/left edge of the canvas
+			ballList[i].color = [random(0, 255), random(0, 255), random(0, 255)]; 		//makes the circle a random color
+			ballList[i].xSpeed = -ballList[i].xSpeed; 									//change the circle speed to make it move left
+		}else if(ballList[i].xCoor < 0) {
+			ballList[i].color = [random(0, 255), random(0, 255), random(0, 255)];		//make the circle a random color
+			ballList[i].xSpeed = -ballList[i].xSpeed; 									//change the circle speed to make it move right
+		}
+		ballList[i].xCoor += ballList[i].xSpeed
+		if(ballList[i].yCoor >= canvas.height){											//makes the circle diameter random
+			ballList[i].diameter = random(10, 80)
+			ballList[i].ySpeed = -ballList[i].ySpeed;									//change the circle speed to make it move down
+		}else if(ballList[i].yCoor < 0) {
+			ballList[i].diameter = random(10, 80)										//make the circle diameter random
+			ballList[i].ySpeed = -ballList[i].ySpeed;									//change the circle speed to make it move up
+		}
+		ballList[i].yCoor += ballList[i].ySpeed
 	}
-	
-	circle.xCoor += circle.xSpeed
-	
-	//if hit the top/bottom edge of the canvas
-	
-	if(circle.yCoor >= 680){	
-		//makes the circle diameter random
-		circle.diameter = random(20, 200)
-		
-		//change the circle speed to make it move down
-		circle.ySpeed = -circle.ySpeed;
-		
-	}else if(circle.yCoor < 0) {
-		//make the circle diameter random
-		circle.diameter = random(20, 100)
-		
-		//change the circle speed to make it move up
-		circle.ySpeed = -circle.ySpeed;
-	}
-	
-	circle.yCoor += circle.ySpeed
-	
-	//up down changes size
-	//left right changes color
-	
 }
