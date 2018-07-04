1adcf2148b9ef15bbba46ed9d238c69dc90d3694
peachy-sky <fluffypuppyw5@gmail.com>
2018-07-04 11:53:36 -0700

    working on some things

--- Week_2/ProcessingJS/sketch.js
+++ Week_2/ProcessingJS/sketch.js
@@ -14,20 +14,32 @@ function Ball(x, y, diameter, color, xSpeed, ySpeed){
 	this.xCoor = x;																		//"this" is a keyword that refers to the individual object
 	this.yCoor = y;
 	this.diameter = diameter;
-	this.color = color || [0, 0, 0];													// || [0, 0, 0] makes the default color black
+	this.color = color || [0, 0, 0];													// || [0, 0, 0] makes the default/start color black
 	this.xSpeed = xSpeed || 12;
 	this.ySpeed = ySpeed || 5;
 }
-
 var circle = new Ball(30, 30, 15);														//define global variables
 var circle2 = new Ball(30, 30, 15);
-var circle3 = new Ball(30, 30, 15);
+var circle3 = new Ball(Math.random() * (width-diameter)+(diameter/2), Math.random() * (height-diameter)+(diameter/2), 15);
 var ballList = [circle, circle2, circle3];
 
 
 function setup() {																		//setup is run once at the start
-	createCanvas(parseInt(prompt("What do you want the canvas's X length to be?")), parseInt(prompt("What do you want the canvas's Y length to be?")));																			//ask what size the user wants the window to be
+	createCanvas(parseInt(prompt("What do you want the canvas's X length to be?")), parseInt(prompt("What do you want the canvas's Y length to be?")));																								//ask what size the user wants the window to be
 }
+
+
+var randomNum = Math.round(Math.random() * 3);											//create a variable that chooses how many circles are drawn
+	if(randomNum == 0){
+		randomNum == 2
+	}
+	else if(randomNum == 1){
+		randomNum == 5
+	}
+	else if(randomNum == 2){
+		randomNum == 9
+	}
+	else randomNum == 16
 
 
 function randomColor() {
@@ -41,7 +53,7 @@ function draw() {
 	for(var i = 0; i < ballList.length; i++){
 		fill(ballList[i].color);
 		ellipse(ballList[i].xCoor, ballList[i].yCoor, ballList[i].diameter);
-		if(ballList[i].xCoor >= canvas.width){											//if hit the right/left edge of the canvas
+		if(ballList[i].xCoor >= width){													//if hit the right/left edge of the canvas
 			ballList[i].color = [random(0, 255), random(0, 255), random(0, 255)]; 		//makes the circle a random color
 			ballList[i].xSpeed = -ballList[i].xSpeed; 									//change the circle speed to make it move left
 		}else if(ballList[i].xCoor < 0) {
@@ -49,7 +61,7 @@ function draw() {
 			ballList[i].xSpeed = -ballList[i].xSpeed; 									//change the circle speed to make it move right
 		}
 		ballList[i].xCoor += ballList[i].xSpeed
-		if(ballList[i].yCoor >= canvas.height){											//makes the circle diameter random
+		if(ballList[i].yCoor >= height){												//makes the circle diameter random
 			ballList[i].diameter = random(10, 80)
 			ballList[i].ySpeed = -ballList[i].ySpeed;									//change the circle speed to make it move down
 		}else if(ballList[i].yCoor < 0) {
