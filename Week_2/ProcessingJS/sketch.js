72fd5672689e7da4bdbd661f1c1e90ebce6cf7e0
peachy-sky <fluffypuppyw5@gmail.com>
2018-07-06 07:35:08 -0700

    circles (2, 5, 10, or 16) change size, color, bounce off walls, user defines canvas size, balls do not leave trail

--- Week_2/ProcessingJS/sketch.js
+++ Week_2/ProcessingJS/sketch.js
@@ -1,54 +1,55 @@
-var ballList = [];
+var ballList = [];																		//create an empty array for the balls that will be filled later
 
 function setup() {																		//setup is run once at the start
-	background(255, 164, 204);
 	createCanvas(parseInt(prompt("What do you want the canvas's X length to be?")), parseInt(prompt("What do you want the canvas's Y length to be?")));
 																						//ask what size the user wants the window to be
-	var circleAmount =random([2, 5, 10, 16]);
+	var circleAmount = random([2, 5, 10, 16]);											//choose randomly 2, 5, 10, or 16, and make that # of circle
 	for( var i = 0; i < circleAmount; i++){
-		ballList[i] = new Ball (random(0, width), random(0, height), 15);
+		ballList[i] = new Ball (random(0, width), random(0, height), 15);				//add the new ball to array ballList
 	}
 }
 
 
-function Ball(x, y, diameter, color, xSpeed, ySpeed){
+function Ball(x, y, diameter, color, xSpeed, ySpeed){									//this is the template for making circles
 	this.xCoor = x;																		//"this" is a keyword that refers to the individual object
 	this.yCoor = y;
 	this.diameter = diameter;
-	this.color = color || [255, 164, 204];													// || [0, 0, 0] makes the default/start color black
+	this.color = color || [255, 164, 204];												// "||" means these properties are optional, and have set defaults
 	this.xSpeed = xSpeed || 12;
 	this.ySpeed = ySpeed || 5;
 }
 
 
-
-/*function collision(ball1, ball2) {
-	var space =;
-	var radii = (ball1.diameter/2)
-	if()
-}*/
-
-
 function draw() {																		//draw the circle
 	background(255,255,255);
-	for(var i = 0; i < ballList.length; i++){
+	for(var i = 0; i < ballList.length; i++){											//every ball will do the following things;
 		fill(ballList[i].color);
 		ellipse(ballList[i].xCoor, ballList[i].yCoor, ballList[i].diameter);
-		if(ballList[i].xCoor >= width){													//if hit the right/left edge of the canvas
+		if(ballList[i].xCoor >= width){													//if hitting the right/left edge of the canvas
 			ballList[i].color = [random(0, 255), random(0, 255), random(0, 255)]; 		//makes the circle a random color
-			ballList[i].xSpeed = -(ballList[i].xSpeed + 1); 									//change the circle speed to make it move left
-		}else if(ballList[i].xCoor < 0) {
-			ballList[i].color = [random(0, 255), random(0, 255), random(0, 255)];		//make the circle a random color
-			ballList[i].xSpeed = -ballList[i].xSpeed + 1; 									//change the circle speed to make it move right
+			ballList[i].xSpeed = -(ballList[i].xSpeed + 1); 							//change the circle speed to make it move left
+		}else if(ballList[i].xCoor < 0) {												
+			ballList[i].color = [random(0, 255), random(0, 255), random(0, 255)];		
+			ballList[i].xSpeed = -ballList[i].xSpeed + 1; 								//change the circle speed to make it move right
+				if (ballList[i].xSpeed > 100) {
+					ballList[i].xSpeed = 12; 
+				} 
 		}
-		ballList[i].xCoor += ballList[i].xSpeed
-		if(ballList[i].yCoor >= height){												//makes the circle diameter random
+		if(ballList[i].yCoor >= height){												//if hitting the top/bottom edge of the canvas;
 			ballList[i].diameter = random(10, 80)
 			ballList[i].ySpeed = -ballList[i].ySpeed;									//change the circle speed to make it move down
 		}else if(ballList[i].yCoor < 0) {
 			ballList[i].diameter = random(10, 80)										//make the circle diameter random
 			ballList[i].ySpeed = -ballList[i].ySpeed;									//change the circle speed to make it move up
 		}
+		
+		
+		ballList[i].xCoor += ballList[i].xSpeed
 		ballList[i].yCoor += ballList[i].ySpeed
 	}
+	/*var space = Math.sqrt(Math.pow((ballList[i].xCoor - ballList[i+1].xCoor), 2) + Math.pow((ballList[i].yCoor - ballList[i+1].yCoor), 2));
+	var bothRadii = (ball1.diameter/2) + (ball2.diameter/2);
+	if(space < bothRadii){
+		ballList[i].xSpeed = -ballList[i].xSpeed
+	}*/
 }
